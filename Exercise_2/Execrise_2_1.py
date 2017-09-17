48e764e4a1e20d06ea74622d524de5bf2978ca00
liangxuwhu <31907112+liangxuwhu@users.noreply.github.com>
2017-09-17 14:14:12 +0800

    Add files via upload

---
+++ Exercise_2/Execrise_2_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 14:11:10 2017
+
+@author: lxwhu
+"""
+
+print ("lll                                xxxx                      xxxx")
+print ("lll                                 xxxx                   xxxx")
+print ("lll                                   xxxx               xxxx")
+print ("lll                                     xxxx           xxxx")
+print ("lll                                       xxxx       xxxx")
+print ("lll                                          xxxx   xxxx")
+print ("lll                                          xxxxxxxx"  )
+print ("lll                                            xxxx")
+print ("lll                                          xxxxxxxx")
+print ("lll                                        xxxx    xxxx")
+print ("lll                                       xxxx        xxxx")
+print ("lll                                    xxxx            xxxx")
+print ("lll                                  xxxx                xxxx")
+print ("lllllllllllllllllll                xxxx                    xxxx")
+print ("lllllllllllllllllll              xxxx                        xxxx")
